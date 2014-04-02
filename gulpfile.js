3a8c29627e3e5d1e403dab9eb53acd37b3f33f20
索丘 <zengyue.yezy@alibaba-inc.com>
2014-04-02 13:30:33 +0800

    bs3

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var git = require('gulp-git');
+var package = require('./package.json');
+
+var branch = 'demo';
+
+gulp.task('pull', function(done){
+  git.pull('origin', branch, {args: '--rebase'}, done);
+});
+
+// Run git add with options
+gulp.task('add', ['pull'], function(){
+  return gulp.src('./')
+  .pipe(git.add());
+});
+
+// Run git commit
+// src are the files to commit (or ./*)
+gulp.task('commit', ['add'], function(){
+  return gulp.src('./')
+  .pipe(git.commit(package.name));
+});
+
+gulp.task('default', ['commit'], function(done){
+  git.push('origin', branch, {args: " -f"}, done);
+});
